2c1b18b1b9f0a4fa05e86a2b5bc19c2a624d13c8
Travis <arma3cloud@gmail.com>
2022-03-06 20:46:57 -0700

    Design plus functionality (not responsive yet)

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
 import './Main.css';
 
 function Main() {
+  const [id, setId] = useState("");
+  const [advice, setAdvice] = useState("");
+
+  function getData() {
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        setId(result.slip.id);
+        setAdvice(result.slip.advice);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="wrapper">
-      <main>
-        
-      </main>
-    </div>
+    <main className="wrapper">
+      <div className='container'>
+        <h1>ADVICE #{id}</h1>
+        <p>{advice}</p>
+        <div className='divider'></div>
+        <button className='dice-button' onClick={getData} />
+      </div>
+    </main>
   );
 }
 
